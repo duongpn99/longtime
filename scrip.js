a77a26c0518519b61727b64fc9339c1ec8e0ca7e
duongpn99 <ngocduong101999@gmail.com>
2023-04-19 10:59:23 +0700

    h
    h

---
+++ scrip.js
@@ -0,0 +1,30 @@
+let ads="hello";
+console.log(ads);
+let meses=" welcome";
+console.log(meses);
+let data;
+console.log(data);
+console.log(typeof(data));
+let object=null;
+console.log(typeof(object));
+let employee={
+    ho:'pham',
+    ten:'ngoc duong'
+};
+console.log(employee.ho);
+console.log(employee.ten);
+let diachi={
+    so:'nha so 4',
+    duong:'cau dien',
+    quan:'bac tu liem',
+    tinh: {
+        thudo:'hanoi',
+        thanhpho:'ho chi minh'
+    }
+};
+console.log(diachi.tinh.thudo)
+function say(message){
+    console.log(message);
+}
+let result= say('hello,world');
+console.log('Result:',result);
